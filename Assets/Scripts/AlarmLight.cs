388ad73c6f2fd33ae04309ed8d16cd898e5ef45f
Kevin Bennett <kbennett2807@gmail.com>
2015-01-12 18:14:48 +0000

    Initial commit

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour
+{		
+	public float fadeSpeed = 2f;
+	public float lowIntensity = 0.5f;
+	public float highIntensity = 2f;
+	public float changeMargin = 0.2f;
+	public bool alarmOn;
+	float targetIntensity;
+
+	void Awake ()
+	{
+		light.intensity = 0f;
+		targetIntensity = highIntensity;
+	}
+
+	void Update ()
+	{
+		if (alarmOn)
+		{
+			FadeToTargetIntensity ();
+			CheckForTargetIntensityToggle();
+		}
+		else
+		{
+			FadeToZeroIntensity ();
+		}
+	}
+
+	void FadeToTargetIntensity ()
+	{
+		light.intensity = Mathf.Lerp (light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToZeroIntensity ()
+	{
+		light.intensity = Mathf.Lerp (light.intensity, 0f, fadeSpeed * Time.deltaTime);
+	}
+
+	void CheckForTargetIntensityToggle ()
+	{
+		if (LightIntensityWithinMarginOfTarget ())
+		{
+			ToggleTargetIntensity ();
+		}
+	}
+
+	bool LightIntensityWithinMarginOfTarget ()
+	{
+		return Mathf.Abs (targetIntensity - light.intensity) < changeMargin;
+	}
+
+	void ToggleTargetIntensity ()
+	{
+		if (targetIntensity == highIntensity)
+		{
+			targetIntensity = lowIntensity;
+		}
+		else
+		{
+			targetIntensity = highIntensity;
+		}
+	}
+}
---
+++ Assets/Scripts/LastPlayerSighting.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LastPlayerSighting : MonoBehaviour
+{
+	public Vector3 resetPosition = new Vector3 (1000f, 1000f, 1000f) ;
+	public Vector3 position = new Vector3 (1000f, 1000f, 1000f);
+	public float lightHighIntensity = 0.25f;
+	public float lightLowIntensity = 0f;
+	public float fadeSpeed = 7f;
+	public float musicFadeSpeed = 1f;
+	AlarmLight alarm;
+	Light mainLight;
+	AudioSource panicAudio;
+	AudioSource[] sirens;
+
+	void Awake ()
+	{
+		alarm = GameObject.FindWithTag (Tags.alarmLight).GetComponent<AlarmLight> ();
+		mainLight = GameObject.FindWithTag (Tags.mainLight).light;
+		panicAudio = transform.Find ("secondaryMusic").audio;
+		GameObject[] sirenGameObjects = GameObject.FindGameObjectsWithTag (Tags.siren);
+		sirens = new AudioSource[sirenGameObjects.Length];
+
+		for (int i = 0; i < sirens.Length; i++)
+		{
+			sirens [i] = sirenGameObjects [i].audio;
+		}
+	}
+
+	void Update()
+	{
+		SwitchAlarms();
+	}
+
+	void SwitchAlarms ()
+	{
+		alarm.alarmOn = PlayerLocationKnown ();
+		CheckMainLightIntensity ();
+		CheckAlarmSounds ();
+	}
+
+	void CheckMainLightIntensity ()
+	{
+		float newIntensity;
+		if (PlayerLocationKnown ())
+		{
+			newIntensity = lightLowIntensity;
+		}
+		else
+		{
+			newIntensity = lightHighIntensity;
+		}
+		mainLight.intensity = Mathf.Lerp (mainLight.intensity, newIntensity, fadeSpeed * Time.deltaTime);
+	}
+
+	void CheckAlarmSounds ()
+	{
+		foreach (AudioSource siren in sirens)
+		{
+			if (PlayerLocationKnown () && !siren.audio.isPlaying)
+			{
+				siren.audio.Play ();
+			}
+			else if (!PlayerLocationKnown ())
+			{
+				siren.Stop ();
+			}
+		}
+	}
+
+	bool PlayerLocationKnown ()
+	{
+		return (position != resetPosition);
+	}
+
+	void CheckMusicFading()
+	{
+		if(PlayerLocationKnown())
+		{
+			audio.volume = Mathf.Lerp(audio.volume, 0f, musicFadeSpeed * Time.deltaTime);
+			panicAudio.volume = Mathf.Lerp(panicAudio.volume, 0.8f, musicFadeSpeed * Time.deltaTime);
+		}
+		else
+		{
+			audio.volume = Mathf.Lerp(audio.volume, 1f, musicFadeSpeed * Time.deltaTime);
+			panicAudio.volume = Mathf.Lerp(panicAudio.volume, 0f, musicFadeSpeed * Time.deltaTime);
+		}
+	}
+}
